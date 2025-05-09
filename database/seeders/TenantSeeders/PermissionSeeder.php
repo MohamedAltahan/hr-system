2fa546bbc3f78db9ae0db8aa17b16e62b50c1634
mohamed altahan <medo85559@gmail.com>
2025-05-09 21:26:22 +0300

    sidebar permissions

--- database/seeders/TenantSeeders/PermissionSeeder.php
+++ database/seeders/TenantSeeders/PermissionSeeder.php
@@ -10,20 +10,11 @@ class PermissionSeeder extends Seeder
 {
     public function run(): void
     {
-        // $permissionNames = collect(config('tenantPermissions'))
-        //     ->flatMap(fn($values, $key) => collect($values)->map(fn($value) => "{$key}_{$value}"))
-        //     ->unique(); //prevent duplicates
+        $modelsPermissions = $this->getModelsPermissions();
 
-        $permissions = collect(config('permissions'))
-            ->flatMap(function ($values, $key) {
-                return collect($values)->map(function ($value) use ($key) {
-                    return [
-                        'name' => "{$key}_{$value}",
-                        'title' => $this->translatePermissionTitle($key, $value),
-                    ];
-                });
-            })
-            ->unique('name'); // prevent duplicates
+        $sidebarPermissions = $this->getSidebarPermissions();
+
+        $permissions = $modelsPermissions->merge($sidebarPermissions);
 
         foreach ($permissions as $permission) {
             Permission::updateOrCreate(
@@ -31,6 +22,7 @@ class PermissionSeeder extends Seeder
                 [
                     'name' => $permission['name'],
                     'title' => $permission['title'],
+                    'group' => $permission['group'],
                     'guard_name' => GuardEnum::TENANTUSER->value,
                 ]
             );
@@ -45,5 +37,40 @@ class PermissionSeeder extends Seeder
         }
 
         return $title;
+    }
+
+    private function getModelsPermissions(): \Illuminate\Support\Collection
+    {
+        return collect(config('permissions'))
+            ->flatMap(function ($values, $key) {
+                return collect($values)->map(function ($value) use ($key) {
+                    return [
+                        'name' => "{$key}_{$value}",
+                        'title' => $this->translatePermissionTitle($key, $value),
+                        'group' => 'model'
+                    ];
+                });
+            })
+            ->unique('name'); // prevent duplicates
+    }
+
+    private function getSidebarPermissions(): \Illuminate\Support\Collection
+    {
+        return collect(config('sidebar'))->flatMap(function ($item) {
+
+            $parent = [
+                'name' => $item['permission_name'],
+                'title' => $item['name'],
+                'group' => 'sidebar',
+            ];
+
+            $children = collect($item['children'] ?? [])
+                ->map(fn($entry) => [
+                    'name' => $entry['permission_name'],
+                    'title' => $entry['name'],
+                    'group' => 'sidebar',
+                ]);
+            return collect([$parent])->merge($children);
+        });
     }
 }
