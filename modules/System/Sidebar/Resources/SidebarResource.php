4772196a619cc1ef5ff6a9eacb90476a5bbb2e77
mohamed altahan <medo85559@gmail.com>
2025-05-22 12:12:56 +0300

    cut route

--- modules/System/Sidebar/Resources/SidebarResource.php
+++ modules/System/Sidebar/Resources/SidebarResource.php
@@ -3,17 +3,23 @@
 namespace Modules\System\Sidebar\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Route;
 
 class SidebarResource extends JsonResource
 {
     public function toArray($request)
     {
+        $fullRoute = $this->route ? route($this->route) : '';
+        $sectionUrl = '';
+
+        if ($fullRoute && preg_match('#/v1/(.+)$#', $fullRoute, $matches)) {
+            $sectionUrl = $matches[1]; // This will capture whatever comes after /v1/
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
-            'route' => $this->route ? route($this->route) : '',
+            'route' => $sectionUrl,
             'icon' => $this->icon,
             'is_active' => $this->is_active,
             'order' => $this->order,
