334dc3825e84594e3ffa402bcb991b5a13dd47e0
mohamed altahan <medo85559@gmail.com>
2025-05-27 01:43:31 +0300

    fix

--- modules/System/User/Models/User.php
+++ modules/System/User/Models/User.php
@@ -70,6 +70,7 @@ class User extends Authenticatable
         return $this->belongsTo(User::class, 'direct_manager_id');
     }
 
+
     public function department()
     {
         return $this->belongsTo(Department::class, 'department_id');
