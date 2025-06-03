3d44047af2f898371c2f42c7281119b5cd5770d9
mohamed altahan <medo85559@gmail.com>
2025-06-03 12:32:51 +0300

    fix port cors

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,7 @@ return [
         'http://localhost:5174',
         'http://localhost:5173',
         'http://127.0.0.1:5174',
+        'http://127.0.0.1:5173',
         'https://alkholoudhr.com',
         'https://www.alkholoudhr.com',
         'http://alkholoudhr.com',
