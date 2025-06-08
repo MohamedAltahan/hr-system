a384865cd60c5510f60d1e04fde24dd5c83a8a0a
mohamed altahan <medo85559@gmail.com>
2025-06-09 02:53:33 +0300

    edit plan_id validation in tenant

--- modules/System/Tenant/Http/Requests/TenantRequest.php
+++ modules/System/Tenant/Http/Requests/TenantRequest.php
@@ -24,23 +24,26 @@ class TenantRequest extends ApiRequest
                 'string',
                 'min:3',
                 'max:30',
-                Rule::unique($centralConnection.'.tenants', 'domain')->ignore($tenant_id),
+                Rule::unique($centralConnection . '.tenants', 'domain')->ignore($tenant_id),
             ],
             'phone' => [
                 'required',
                 'string',
                 'min:8',
                 'max:15',
-                Rule::unique($centralConnection.'.tenants', 'phone')->ignore($tenant_id),
+                Rule::unique($centralConnection . '.tenants', 'phone')->ignore($tenant_id),
             ],
             'email' => [
                 'required',
                 'email',
                 'min:3',
                 'max:50',
-                Rule::unique($centralConnection.'.tenants', 'email')->ignore($tenant_id),
+                Rule::unique($centralConnection . '.tenants', 'email')->ignore($tenant_id),
             ],
-            'plan_id' => "required|exists:$centralConnection.plans,id",
+            'plan_id' => [
+                $tenant_id ? 'sometimes' : 'required',
+                "exists:$centralConnection.plans,id",
+            ],
             'is_active' => 'boolean',
 
         ];
