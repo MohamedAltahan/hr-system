b6e7aa5ba56a7daef68f8373857f0f9129794365
mohamed altahan <medo85559@gmail.com>
2025-07-05 12:13:48 +0300

    add requests types

--- modules/System/Employee/EmployeeRequest/Enum/EmployeeRequestTypeEnum.php
+++ modules/System/Employee/EmployeeRequest/Enum/EmployeeRequestTypeEnum.php
@@ -4,11 +4,16 @@ namespace Modules\System\Employee\EmployeeRequest\Enum;
 
 enum EmployeeRequestTypeEnum: string
 {
-    case LOAN = 'loan';
+    case ATTENDANCE_CORRECTION = 'attendance_correction';
+    case ADVANCE = 'advance';
     case LEAVE = 'leave';
     case LATE_ARRIVAL = 'late_arrival';
     case EARLY_DEPARTURE = 'early_departure';
-    case OTHER = 'other';
+    case OVER_TIME = 'over_time';
+    case RESIGNATION = 'resignation';
+    case ASSET = 'asset';
+    case ASSET_CLEARANCE = 'asset_clearance';
+    case LETTERS = 'letters';
 
     public function label(): string
     {
